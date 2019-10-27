93099fcabb39fde41df07d99ee04c1daaded5e3f
Ke Xu <kdotxu@gmail.com>
2019-10-26 18:32:21 -0700

    Drawing the '@' symbol and moving it around

--- src/yarl/core.cljs
+++ src/yarl/core.cljs
@@ -1,11 +1,77 @@
 (ns yarl.core
   (:require
-    [reagent.core :as r]))
+    ["rot-js" :as rot]))
 
-(defn app [] [:div "textbook-roguelike is running!"])
 
-(defn ^:dev/after-load start
-  []
-  (r/render [app] (.getElementById js/document "app")))
+(defonce game (atom nil))
 
-(defn ^:export main [] (start))
+(reset!
+  game
+  {:player
+    {:x 1
+     :y 1
+     :symbol "@"}})
+
+
+(defn move-player [state [dx dy]]
+  (-> state
+    (update-in [:player :x] + dx)
+    (update-in [:player :y] + dy)))
+
+
+(defn handle-input [state event]
+  (let [code (.-keyCode event)]
+    (cond
+      (= (.-type event) "keyup") state ;; don't move
+      (= code (.-VK_UP rot/KEYS)) (move-player state [0 -1])
+      (= code (.-VK_DOWN rot/KEYS)) (move-player state [0 1])
+      (= code (.-VK_LEFT rot/KEYS)) (move-player state [-1 0])
+      (= code (.-VK_RIGHT rot/KEYS)) (move-player state [1 0])
+      :else state)))
+
+
+(defn update-state [state dt event]
+  (js/console.log (.-type event))
+  (if event
+    (handle-input state event)
+    state))
+
+
+(defn update-game [event]
+  (prn "game state:" (swap! game update-state nil event)))
+
+
+(defn addEventListener [event]
+  (.addEventListener js/window event #(update-game %)))
+
+
+(defn register-input-handler []
+  (addEventListener "keydown")
+  (addEventListener "keyup")
+  (addEventListener "keypress"))
+
+(defn render-player [display {:keys [x y symbol] :as player}]
+  (doto display
+    (.draw x y symbol)))
+
+
+(defn render [display state]
+  (.clear display)
+  (render-player display (:player state)))
+
+
+(defn init []
+  (let [display (rot/Display. #js {"fontFamily" "Menlo"})
+        el (.getContainer display)]
+    (.. js/document -body (appendChild el))
+    (letfn [(render-loop []
+                (render display @game)
+                (js/requestAnimationFrame render-loop))]
+      (render-loop))
+    (register-input-handler)))
+
+
+(defn ^:dev/after-load start []
+  nil)
+
+(defn ^:export main [] (init))
